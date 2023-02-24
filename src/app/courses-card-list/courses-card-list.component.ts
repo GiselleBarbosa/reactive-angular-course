0dcb30654b78771783cfedc3bfbe7eaa35c0c427
GiselleBarbosa <g88barbosa@gmail.com>
2023-02-24 14:42:49 -0300

    style: fix all files format

---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { Observable } from "rxjs";
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.scss"],
+})
+export class CoursesCardListComponent implements OnInit {
+  @Input() advancedCourses$: Observable<Course[]>;
+
+  courses: Course[] = [];
+
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  editCourse(course: Course) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+  }
+}
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,25 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Course } from '../model/course';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>("/api/courses")
-      .pipe(
-        map(res => res["payload"]),
-        shareReplay()
-      );
-
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((res) => res["payload"]),
+      shareReplay()
+    );
   }
-
 }
