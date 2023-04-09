22f14a58b494674e469e5acf00280fd87ca60c7f
GiselleBarbosa <g88barbosa@gmail.com>
2023-04-09 15:30:20 -0300

    feat: auth works

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,11 +4,14 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data";
+
 @Injectable({
   providedIn: "root",
 })
 export class AuthStore {
   private subject = new BehaviorSubject<User>(null);
+
   user$: Observable<User> = this.subject.asObservable();
 
   isLoggedIn$: Observable<boolean>;
@@ -17,17 +20,27 @@ export class AuthStore {
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
 
-    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !!loggedIn));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
+    const user = localStorage.getItem(AUTH_DATA);
+
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
   }
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>("/api/login", { email, password }).pipe(
-      tap((user) => this.subject.next(user)),
+      tap((user) => {
+        this.subject.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
 
-  logout(email: string, password: string) {
+  logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 }
